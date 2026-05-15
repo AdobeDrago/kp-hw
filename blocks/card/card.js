cdc32ca1e7857d30c2542a82c3fc57607b73d8b7
timwlee <timothyl@adobe.com>
2026-05-15 13:30:33 -0700

    Update card block

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -28,4 +28,11 @@ export default function init(el) {
   }
   ctaPara.classList.add('card-cta-container');
   inner.append(ctaPara);
+
+  const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  icon.setAttribute('viewBox', '0 0 16 16');
+  icon.setAttribute('aria-hidden', 'true');
+  icon.classList.add('card-external-icon');
+  icon.innerHTML = '<path d="M13 9v4a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4V1H3a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h9a3 3 0 0 0 3-3V9h-2zm-3-8v2h2.586L6.293 9.293l1.414 1.414L14 4.414V7h2V1h-6z"/>';
+  cta.append(icon);
 }
